775d90f12ee591bff1d921c1ec7d50a1090102d9
gsalomone <gianfranco.s@gmail.com>
2024-05-01 09:22:04 -0300

    Add locust.conf and simplify locust command

--- src/locustfile.py
+++ src/locustfile.py
@@ -3,17 +3,13 @@ import random
 
 from typing import List
 
-from dotenv import dotenv_values
 from locust import HttpUser, task, constant
 
 from grpc_handlers import create_channel, vacancy_crud, user_signin
 
-VACANCY_SERVER_URL = dotenv_values().get('VACANCY_SERVER_URL')
-
 
 class VacancyUser(HttpUser):
     wait_time = constant(30)
-    host = VACANCY_SERVER_URL
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
