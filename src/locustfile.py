f9aa5de8e8a4ab90c980305213532743500db08e
gsalomone <gianfranco.s@gmail.com>
2024-05-01 08:52:43 -0300

    wait_time attribute is now constant()

--- src/locustfile.py
+++ src/locustfile.py
@@ -4,7 +4,7 @@ import random
 from typing import List
 
 from dotenv import dotenv_values
-from locust import HttpUser, task
+from locust import HttpUser, task, constant
 
 from grpc_handlers import create_channel, vacancy_crud, user_signin
 
@@ -12,7 +12,7 @@ VACANCY_SERVER_URL = dotenv_values().get('VACANCY_SERVER_URL')
 
 
 class VacancyUser(HttpUser):
-    wait_time = 30
+    wait_time = constant(30)
     host = VACANCY_SERVER_URL
 
     def __init__(self, *args, **kwargs):
